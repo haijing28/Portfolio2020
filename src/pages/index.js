c3387f445c03067c85728670e07c57596cddbfa1
Haijing <liuh494@newschool.edu>
2020-02-13 23:50:28 +0800

    bannerupdated
    love+

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
-import love from '../assets/images/love01.jpg'
+import love from '../assets/images/love.png'
 import smty from '../assets/images/love02.jpg'
 import colm from '../assets/images/pic03.jpg'
 import trnd from '../assets/images/pic04.jpg'
@@ -30,7 +30,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Designpholio"
+                    title="DesignLens"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
@@ -46,80 +46,82 @@ class HomeIndex extends React.Component {
                     <section id="one" className="tiles">
 
                      
-                        <article style={{backgroundImage: `url(${love})`}}>
-                            <header className="major">
-                                <h3>Love+</h3>
-                                <p>hardware as a service, powered by electrolux</p>
-                            </header>
-                            <Link to="/love+" className="link primary"></Link>
-                        </article> 
+                                <article style={{backgroundImage: `url(${love})`}}>
+                                    <header className="major">
+                                        <h3>Love+</h3>
+                                        <p>hardware as a service, powered by electrolux</p>
+                                    </header>
+                                    <Link to="/love+" className="link primary"></Link>
+                                </article> 
 
-                   
-                      
-                        <article style={{backgroundImage: `url(${smty})`}}>
-                            <header className="major">
-                                <h3>Smartify</h3>
-                                <p>Midea Product Innovation for Millennial Life Style</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-
-
-                        <article style={{backgroundImage: `url(${colm})`}}>
-                            <header className="major">
-                                <h3>Colmo UX</h3>
-                                <p> Premium Appliance UX Strategy & Design</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
+                           
+                              
+                                <article style={{backgroundImage: `url(${smty})`}}>
+                                    <header className="major">
+                                        <h3>Smartify</h3>
+                                        <p>Midea Product Innovation for Millennial Life Style</p>
+                                    </header>
+                                    <Link to="/landing" className="link primary"></Link>
+                                </article>
 
 
-                        <article style={{backgroundImage: `url(${trnd})`}}>
-                            <header className="major">
-                                <h3>Midea Trend Research</h3>
-                                <p>Design Trend HandBook for Midea Group</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
+                                <article style={{backgroundImage: `url(${colm})`}}>
+                                    <header className="major">
+                                        <h3>Colmo UX</h3>
+                                        <p> Premium Appliance UX Strategy & Design</p>
+                                    </header>
+                                    <Link to="/landing" className="link primary"></Link>
+                                </article>
 
 
-                        <article style={{backgroundImage: `url(${spch})`}}>
-                            <header className="major">
-                                <h3>Spacechat</h3>
-                                <p> Location Based Service </p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
+                                <article style={{backgroundImage: `url(${trnd})`}}>
+                                    <header className="major">
+                                        <h3>Midea Trend Research</h3>
+                                        <p>Design Trend HandBook for Midea Group</p>
+                                    </header>
+                                    <Link to="/landing" className="link primary"></Link>
+                                </article>
 
 
-                        <article style={{backgroundImage: `url(${wsmg})`}}>
-                            <header className="major">
-                                <h3>Wish Message</h3>
-                                <p>MFA Design & Technology Thesis</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
+                                <article style={{backgroundImage: `url(${spch})`}}>
+                                    <header className="major">
+                                        <h3>Spacechat</h3>
+                                        <p> Location Based Service </p>
+                                    </header>
+                                    <Link to="/landing" className="link primary"></Link>
+                                </article>
 
 
-                        <article style={{backgroundImage: `url(${hncb})`}}>
-                            <header className="major">
-                                <h3>Honeycomb</h3>
-                                <p>Product Innovation @Mozilla NYC Lab </p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
+                                <article style={{backgroundImage: `url(${wsmg})`}}>
+                                    <header className="major">
+                                        <h3>Wish Message</h3>
+                                        <p>MFA Design & Technology Thesis</p>
+                                    </header>
+                                    <Link to="/landing" className="link primary"></Link>
+                                </article>
 
 
-                         <article style={{backgroundImage: `url(${spyt})`}}>
-                            <header className="major">
-                                <h3>Sprayit</h3>
-                                <p> Real Time Collaborative Doodling Tool </p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
+                                <article style={{backgroundImage: `url(${hncb})`}}>
+                                    <header className="major">
+                                        <h3>Honeycomb</h3>
+                                        <p>Product Innovation @Mozilla NYC Lab </p>
+                                    </header>
+                                    <Link to="/landing" className="link primary"></Link>
+                                </article>
 
-                        
+
+                                 <article style={{backgroundImage: `url(${spyt})`}}>
+                                    <header className="major">
+                                        <h3>Sprayit</h3>
+                                        <p> Real Time Collaborative Doodling Tool </p>
+                                    </header>  
+                                    <Link to="/landing" className="link primary"></Link>
+                                </article>
+
                     </section>
+
+
+
 
                     <section id="two">
                         <div className="inner">
